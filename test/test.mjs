bd1a80c9f92ce85d2e672b9e31cf2be76abae1dd
Anatoli Radulov <anatoli+anatolipr@radulov.com>
2025-02-21 07:56:33 -0800

    content update

--- test/test.mjs
+++ test/test.mjs
@@ -1,3 +1,5 @@
+
+
 const imgEl = document.createElement('img');
-imgEl.setAttribute('src', './test.jpg');
+imgEl.setAttribute('src', import.meta.resolve('./test.jpg'));
 document.body.appendChild(imgEl);

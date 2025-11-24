4a8a9fb6fb664a0dba2fafa0a1b0ba86b134bfaa
anatolipr <anatoli@impact.com>
2025-11-23 21:20:03 -0800

    e

--- rand/util.mjs
+++ rand/util.mjs
@@ -3,3 +3,34 @@ export class EventEmitter extends EventTarget {
         this.dispatchEvent(new CustomEvent(eventName, { detail }))
     }
 }
+
+
+function observe(obj) {
+    return new Proxy(obj, {
+        get(target, prop, receiver) {
+            const value = Reflect.get(target, prop, receiver);
+            // Binds methods (like addEventListener) to the original target
+            if (typeof value === 'function') {
+                return value.bind(target);
+            }
+            return value;
+        },
+        set(target, prop, value) {
+            target[prop] = value;
+            
+            console.log(`${prop}Set`)
+            target.dispatchEvent(new CustomEvent(`${prop}Set`, {
+                detail: value
+            }));
+            
+            return true;
+        }
+    });
+}
+
+export class BaseModel extends EventTarget {
+    constructor() {
+        super();
+        return observe(this);
+    }
+} 
